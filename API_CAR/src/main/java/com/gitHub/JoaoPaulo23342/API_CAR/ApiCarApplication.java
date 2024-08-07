3753f1ff815903e6b6a0baeae5ad4d246414a489
João <Joaopauloresendecosta55@gmail.com>
2024-08-07 14:42:08 -0300

    Fist commit

---
+++ API_CAR/src/main/java/com/gitHub/JoaoPaulo23342/API_CAR/ApiCarApplication.java
@@ -0,0 +1,13 @@
+package com.gitHub.JoaoPaulo23342.API_CAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCarApplication.class, args);
+	}
+
+}
---
+++ API_CAR/src/test/java/com/gitHub/JoaoPaulo23342/API_CAR/ApiCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitHub.JoaoPaulo23342.API_CAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
